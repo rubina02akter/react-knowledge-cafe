28d851fd69cc57e9f58ac3ef0cfee7dfa08861b8
rubina02akter <rubinaakter0108@gmail.com>
2024-10-23 00:49:00 +0600

    fake data commit

--- react-knowledge-cafe/src/App.jsx
+++ react-knowledge-cafe/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
-
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
 function App() {
 
   return (
     <>
-      <h1 className='text-3xl bg-red-300'>Knowledge Cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
